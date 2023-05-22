535f7abe6adab0eb280728f65ffc588abae93733
swapnilgaikwad18 <swapnil1329g@gmail.com>
2023-05-23 00:09:38 +0530

    user-defined component (tag)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-//Display Hello World from App components
+//ii) A user-defined component (tag) to display Hello World.
+//Display this tag from App component
 
 function App(){
-  return <h1>Hello World!</h1>
+  return(
+    <div>
+      <Display/>
+    </div>);
+}
+
+function Display(){
+  return(<div><h1>Hello World!!!</h1></div>);
 }
 
 export default App;
